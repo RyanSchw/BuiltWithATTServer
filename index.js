9238ec29ab5c192da8e3b1051763187ad48c201c
Ryan Schwartz <schwar95@purdue.edu>
2019-09-14 10:17:24 -0400

    Send ws post to client on /api/doorEntry

--- index.js
+++ index.js
@@ -44,15 +44,16 @@ const server = express()
     .use((req, res) => res.sendFile(INDEX))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
+app.post('/api/doorEntry', (req, res) => {
+    wss.clients.forEach((client) => {
+        client.send('');
+    });
+    res.sendStatus(200);
+});
+
 const wss = new SocketServer({ server });
 
 wss.on('connection', (ws) => {
     console.log('Client connected');
     ws.on('close', () => console.log('Client disconnected'));
 });
-
-setInterval(() => {
-    wss.clients.forEach((client) => {
-        client.send(new Date().toTimeString());
-    });
-}, 1000);
